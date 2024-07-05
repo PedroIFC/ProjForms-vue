abfa5f16a6eb71987db4e74e894676a90c632ba8
PedroIFc <ps404412@gmail.com>
2024-07-05 00:44:38 -0300

    chore: css formulario.vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+      '70/1': '70%',
+      '40/1': '40%',
+      '50/1': '50%',
+      '45/1': '45%',
+      '5/1': '5%',
+      '10/1': '10%'
+      },
+      height: {
+      '95/1': '95%'
+      },
+      flexBasis:{
+        '50/1': '50%'
+      },
+      gap:{
+        '5/1': '5%',
+        '10/1': '10%'
+      },
+      fontFamily: {
+        'poppins': '"Poppins", sans-serif'
+      }
+    },
   },
   plugins: [],
 }
